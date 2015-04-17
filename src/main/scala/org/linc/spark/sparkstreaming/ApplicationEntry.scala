8c87480b8c8f3c7ae923eef9b80a2f3cc15f9ea2
ihainan <ihainan72@gmail.com>
2015-04-17 15:43:53 +0800

    Add: 将输入和输出放入到一个窗口 RDD 内，foreachRDD 内部解析可同时得到两张表。

--- src/main/scala/org/linc/spark/sparkstreaming/ApplicationEntry.scala
+++ src/main/scala/org/linc/spark/sparkstreaming/ApplicationEntry.scala
@@ -2,7 +2,7 @@ package org.linc.spark.sparkstreaming
 
 import java.util
 
-import org.apache.spark.sql.SQLContext
+import org.apache.spark.sql._
 import org.apache.spark.streaming.kafka.KafkaUtils
 import org.apache.spark.streaming.{Duration, Seconds, StreamingContext}
 import org.apache.spark.{SparkConf, SparkContext}
@@ -18,84 +18,128 @@ import scala.collection.JavaConversions._
 
 
 object ApplicationEntry {
-  //使用窗口操作
+  // 窗口大小
   val WINDOW_LENGTH = new Duration(GlobalConf.windowInterval.get.toLong)
+  // 滑动间隔
   val SLIDE_INTERVAL = new Duration(GlobalConf.slideInterval.get.toLong)
-  val inputAndOutputFormat = new InputAndOutputFormat()
   // 输入输出格式
-  val rules = new Rules(inputAndOutputFormat) // 转换规则
+  val inputAndOutputFormat = new InputAndOutputFormat()
+  // 转换规则
+  val rules = new Rules(inputAndOutputFormat)
 
+  /**
+   * 程序入口
+   * @param args 参数列表
+   */
   def main(args: Array[String]) {
     /* 初始化 */
-    // Spark 系统上下文 ß
     val sparkConf = new SparkConf().setAppName("KafkaWordCount")
     val sc = new SparkContext(sparkConf)
     val ssc = new StreamingContext(sc, Seconds(GlobalConf.batchInterval.get.toLong))
     val sqlContext = new SQLContext(sc)
 
-    // 读取输入数据
+    /* 从 Kafka 中读取输入数据 */
     val topicMap = GlobalConf.topics.get.split(",").map((_, GlobalConf.numThreads.get.toInt)).toMap
     val lines = KafkaUtils.createStream(ssc, GlobalConf.zkQuorum.get, GlobalConf.group.get, topicMap).map(_._2)
 
     /* 字段规则 */
-    // 应用转换规则
     val splitRDD = lines.map(x => inputAndOutputFormat.splitInputIntoHashMap(x))
-    val resultRDD = splitRDD.map(x => rules.applyRules(x))
+    val inputArrayRDD = lines.map(x => new util.ArrayList(inputAndOutputFormat.splitInputIntoHashMap(x).values()))
+    val outputArrayRDD = splitRDD.map(x => rules.applyRules(x))
 
-    // 输出结果到 HDFS
-    resultRDD.foreachRDD(allResult => {
+    /* 输出结果到文件系统当中 */
+    outputArrayRDD.foreachRDD(allResult => {
       if (allResult.count() > 0) {
-        val finalRDD = allResult.map(result => Rules.resultToStr(result))
-        finalRDD.collect().foreach(println)
+        val finalRDD = allResult.map(result => result) // Nothing to do
+        // finalRDD.collect().foreach(println)
         finalRDD.saveAsTextFile(GlobalConf.outputPath.get)
       }
     })
 
-    /* SQL 查询 */
-    // 创建 Schema
+    /* 创建 Schema */
     import org.apache.spark.sql._
-    def getType(t: String): DataType = {
-      if (t == "String") {
-        StringType
-      }
-      else if (t == "Float") {
-        FloatType
-      }
-      else if (t == "Double") {
-        DoubleType
-      }
-      else if (t == "Integer") {
-        IntegerType
-      }
-      StringType
-    };
-    val inputValueRDD = lines.map(x => new util.ArrayList(inputAndOutputFormat.splitInputIntoHashMap(x).values()))
-
     val inputSchema =
-      StructType(inputAndOutputFormat.getInputFormat.keySet().map(key => StructField(key,
-        getType(inputAndOutputFormat.getInputFormat.get(key)), true)).toSeq)
+      StructType(inputAndOutputFormat.getInputFormat.keySet().toArray().map(key => {
+        // println("Key =" + key)
+        StructField(key.toString,
+          getType(inputAndOutputFormat.getInputFormat.get(key)), true)
+      }).toSeq) // input 表 Schema
+    val outputSchema =
+      StructType(inputAndOutputFormat.getOutputFormat.keySet().toArray().map(key =>
+        StructField(key.toString,
+          getType(inputAndOutputFormat.getOutputFormat.get(key)), true))) // output 表 Schema
 
-    // 窗口
-    val windowInputDStream = inputValueRDD.window(WINDOW_LENGTH, SLIDE_INTERVAL)
+    /* 窗口 */
+    val tmpInputTupleRDD = inputArrayRDD.map(x => (1, x))
+    val tmpOutputTupleRDD = outputArrayRDD.map(x => (2, x))
+    val combinedInputAndOutputRDD = tmpInputTupleRDD.union(tmpOutputTupleRDD)
+    // combinedInputAndOutputRDD.foreach(x => println(x.collect().foreach(println)))
+    val windowDStream = combinedInputAndOutputRDD.window(WINDOW_LENGTH, SLIDE_INTERVAL)
 
-    // SQL 查询
-    windowInputDStream.foreachRDD(allInputValue => {
-      if (allInputValue.count() == 0) {
-        println("No input data received in this time interval")
-      } else {
-        val rowRDD = allInputValue.map(result => {
-          val row = Row.fromSeq(result.toIndexedSeq)
+    /* 执行 SQL 查询 */
+    windowDStream.foreachRDD(originalTupleRDD => {
+      if (originalTupleRDD.count() == 0) {
+        println("No data in this time interval")
+      }
+      else {
+        // 注册 input 表并往里插入数据
+        val inputRowRDD = originalTupleRDD.filter(x => x._1 == 1).map(rowTuple => {
+          val row = Row.fromSeq(rowTuple._2)
           row
         })
+        val inputSchemaRDD = sqlContext.applySchema(inputRowRDD, inputSchema)
+        inputSchemaRDD.registerTempTable("input")
 
-        val schemaRDD = sqlContext.applySchema(rowRDD, inputSchema)
-        schemaRDD.registerTempTable("input")
-        // val contentSizeStats = sqlContext.sql(GlobalConf.sql.get) // SQL 操作
-        // contentSizeStats.collect().foreach(println)
+        // 注册 output 表并往里插入数据
+        val outputRowRDD = originalTupleRDD.filter(x => x._1 == 2).map(rowTuple => {
+          val row = Row.fromSeq(rowTuple._2)
+          row
+        })
+        val outputSchemaRDD = sqlContext.applySchema(outputRowRDD, outputSchema)
+        outputSchemaRDD.registerTempTable("output")
+
+        // 执行 SQL 查询
+        println("Count = " + inputRowRDD.count)
+        val contentSizeStats = sqlContext.sql(GlobalConf.sql.get)
+        println("SQL Query Result: ")
+        contentSizeStats.collect().foreach(println)
       }
     })
 
     ssc.start()
     ssc.awaitTermination()
+  }
+
+  /**
+   * 根据字符串获取相应的字段类型
+   * @param t 类型
+   * @return 对应的字段类型
+   */
+  def getType(t: String): DataType = {
+    if (t == "String") {
+      return StringType
+    }
+    else if (t == "Float" || t == "float") {
+      return FloatType
+    }
+    else if (t == "Double" || t == "double") {
+      return DoubleType
+    }
+    else if (t == "Integer" || t == "int") {
+      return IntegerType
+    }
+    else if (t == "Long" || t == "long") {
+      return LongType
+    }
+    else if (t == "Boolean" || t == "boolean") {
+      return BooleanType
+    }
+    else if (t == "Byte" || t == "byte") {
+      return ByteType
+    }
+    else if (t == "Short" || t == "short") {
+      return ShortType
+    }
+    StringType
   }
 }
